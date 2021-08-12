04594e74cb5257ffe021913ca83ee049e96eb2a0
Tor Krill <tor@openproducts.se>
2021-08-12 08:36:59 +0200

    Make sure we have /etc/mailname since roundcube package expects that
    Signed-off-by: Tor Krill <tor@openproducts.se>

--- kgp2/00-setup-kinguard/00-run-chroot.sh
+++ kgp2/00-setup-kinguard/00-run-chroot.sh
@@ -4,16 +4,13 @@ echo "---------------------------------------------"
 echo "              Kinguard setup start"
 echo "---------------------------------------------"
 
-echo "Kinguard: Current path is: [$PATH]"
-
 export PATH=/usr/local/bin/:$PATH
-
-echo "Kinguard: Updated path is: [$PATH]"
 
 apt update && apt -y upgrade
 
-echo "Kinguard: mt"
 apt -y purge exim4 exim4-base exim4-config exim4-daemon-light
+
+echo "kgp-pi" > /etc/mailname
 
 echo "Kinguard: install kinguard package"
 apt -y install kinguard nextcloud21 || true
@@ -24,9 +21,6 @@ rm  -f /etc/apt/sources.list.d/kgp-tmp.list
 rm -f /etc/apt/preferences.d/kgp-pin-installer.pref
 touch /etc/opi/firstboot
 
-#echo "Kinguard: running postsetup"
-#/usr/share/opi-postsetup/kgp-postsetup
-#echo "Kinguard: done postsetup"
 
 echo "---------------------------------------------"
 echo "             Kinguard setup end"
